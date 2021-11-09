d734a3d84da710fd510284374205da9d6520949c
Adarsh Kumar <adarsh08kumar@gmail.com>
2021-11-09 13:09:58 +0530

    Initial Commit

---
+++ home/urls.py
@@ -0,0 +1,10 @@
+
+from django.urls import path, include
+from .views import *
+
+urlpatterns = [
+    path('', home , name="home"),
+    path('bills/', bills , name="bills"),
+    path('past-expenditure/', past_expenditure , name="past_expenditure"),
+   
+]
